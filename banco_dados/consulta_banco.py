db689da16f8c5208a8ffcfddca562026a0b246d0
matheus.oliveira <matheus.barbosa.oliveira00@gmail.com>
2021-10-14 10:18:49 -0300

    Criado codigo que acessa o banco de dados dominio.

---
+++ banco_dados/consulta_banco.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+banco = pyodbc.connect('DSN=Contabil')
+cursor = banco.cursor()
+
+cursor.tables()
+rows = cursor.fetchall()
+# for row in rows:
+#     print(row.table_name)
+
+cursor.execute(
+    "SELECT * FROM externo.bethadba.foempregados WHERE codi_emp = 221"
+)
+tabela = cursor.fetchall()
+print(tabela)
